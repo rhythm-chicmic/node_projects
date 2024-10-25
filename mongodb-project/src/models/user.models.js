08237603bd2029f7b1f82f66cb553fc24d3b4f00
rhythm-chicmic <rhythm.sharma@chicmic.co.in>
2024-10-25 10:37:33 +0530

    feat(): Changes in user Models

--- mongodb-project/src/models/user.models.js
+++ mongodb-project/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
     username: { type: String, required: true, unique: true, lowercase: true, trim: true, index: true },
@@ -23,7 +25,22 @@ const userSchema = new Schema({
     {
         timestamps: true
     }
-
 );
+
+userSchema.pre("save", async function (next){
+    if(!this.modified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
+
+    next()
+});
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function (){
+    //  short lived access token
+
+}
 
 export const User = mongoose.model("User",userSchema);
